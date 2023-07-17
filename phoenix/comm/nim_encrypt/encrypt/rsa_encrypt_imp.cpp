a4ef3b068801af2ebf87ba7c80f884f57f09b3e3
cqu227hk <cqu227hk@163.com>
2023-07-17 10:11:51 +0800

    Update rsa_encrypt_imp.cpp
    删除无用的注释代码

--- phoenix/comm/nim_encrypt/encrypt/rsa_encrypt_imp.cpp
+++ phoenix/comm/nim_encrypt/encrypt/rsa_encrypt_imp.cpp
@@ -81,34 +81,6 @@ int password_callback(char *buf, int bufsiz, int verify, void *cb_tmp)
 }
 bool RSAEncryptMethodImp::PrivateKeyDecrypt(const std::string & raw_content, std::string & decrypted_content)
 {
-//	std::string temp("-----BEGIN PRIVATE KEY-----\n\
-//MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAKT + 2aMeo6sD0WT +\n\
-//ii1 + ne3I9hV1XMwa / rzb7VVHiRN6KVuKs722mOX10l91fMUP8iMQLkLvOY5tViLS\n\
-//mr6grQCKdsUC / UEmndMevE97Dfbzc2nwG + 2j32lDeOBhAmbNAkgyqtNpd / O3qJz4\n\
-//wYMxpKPkuPdiqnXP2mVzgoy29i6ZAgMBAAECgYAvmP172yjn + s / 7TJrcCPzSTfj /\n\
-//s72ze84k + WT8Bwj6P3GS4O59OiHdpQ6tTx1RVApMJyzRTmXSOqRwo0TU3VFKsfmK\n\
-//nJqBeWSt03w3l3ErfdiXiUIoaqyuLMjQok8gc7hLex281Kfr9yFFLL4iJ + ssjLaZ\n\
-//yokJnj765CKEIM9oqQJBANYcwKynkQPdpQd1EwSvxv5M8jYkedL7iCHMJB26k51u\n\
-//aZ / O30qg1emO6k + zVkoCSOeEFgUDZgykVPoPLPnkcxsCQQDFRjZooGeIMgN2cQkD\n\
-//Lj2etJlWHy9lr / P11 / 4ASNf + r4fCQxtv8FIcrHmEv6eHSa9Le7D0wVsBFX9GtVBB\n\
-//1AxbAkEAgQ4HnInLwLbCGAKbGgIRQYqDm9rUizUNUsHHKxwgiY3ZoHeC42qaRRLd\n\
-//bHi + FBHYe6OD2jW4Nvp + NdMb2uu17QJAYf3QhQQ4qVR + l9TqLyRHA5CeIkN3XcGb\n\
-//I2FZEHceo6o5e2uuM1piz091RVWWOAOICay1Z76sH8u33z2tUiWsxQJAHVKOS / +v\n\
-//XNUcAUwzQqZlEmzBFzm6qKAx8sMemjLvnvOxgKMlxd3rDXg8bJflLgvdk8XifIfB\n\
-//K2FDW5Pdi3ShXw ==\n\
-//-----END PRIVATE KEY-----\n");	
-//	std::string temp_private;
-//	temp_private.resize(128);
-//	for (int index = 0; index < 128; index++)
-//		sscanf_s(private_key_.substr(2 * index, 2).c_str(), "%x", (unsigned int*)&temp_private[index]);
-//	unsigned char *rsaPrivateKeyChar = (unsigned char *)temp.c_str();
-//	BIO* bio = BIO_new_mem_buf(rsaPrivateKeyChar, -1);
-//	BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
-//	RSA *rsa;	
-//	rsa = PEM_read_bio_RSAPrivateKey(bio, nullptr, nullptr, nullptr);
-//	EVP_PKEY* key = EVP_PKEY_new();
-//	d2i_PrivateKey_bio(bio, &key);
-//	PEM_read_bio_PrivateKey(bio, &key, nullptr, nullptr);
 	decrypted_content.clear();
 	BIGNUM *bnn, *bne, *bnd, *bnexp;
 	bnn = BN_new();
